87887c82ac73f4628124becfa9fc68ca1908fac4
jesus <jesusalas18@gmail.com>
2017-09-05 23:10:05 -0600

    Se agrego uso de strings y soporte a cambio de idioma

--- app/src/main/java/mx/com/diossa/todoapp/MainPerson.java
+++ app/src/main/java/mx/com/diossa/todoapp/MainPerson.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,20 +20,17 @@ public class MainPerson extends Activity{
         setContentView(R.layout.person_xml);
 
         Button btn_calculate= (Button)findViewById(R.id.btn_calculate_debt);
-        final TextView response = (TextView)findViewById(R.id.response);
+        final EditText salary = (EditText)findViewById(R.id.txt_salary);
+        final TextView answer = (TextView)findViewById(R.id.answer);
 
         btn_calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Person jesus = new Person("jesus","salas",24,60,50);
-
-                Toast.makeText(getApplicationContext(),response.getText() ,Toast.LENGTH_SHORT).show();
-                double olvidar = Double.parseDouble(response.getText().toString());
-                jesus.setWeight(olvidar);
-
-
-                String person = "El maximo de deudas soportadas son " + jesus.calculateIMC();
-                response.setText(person);
+                Person jesus = new Person("jesus","salas",24,60,1.62);
+                Double salary_real = Double.parseDouble(salary.getText().toString());
+                jesus.setWeight(salary_real);
+                String person = getString(R.string.debt_answer) + jesus.calculateIMC() ;
+                answer.setText(person);
             }
         });
 
