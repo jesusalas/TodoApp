bbcdbe0ce42b089e8f675e2ccf762d7c40e9d287
jesus <jesusalas18@gmail.com>
2017-09-09 12:54:53 -0600

    Se crearon los procesos de vida de un activity

--- app/src/main/java/mx/com/diossa/adeudosapp/MainPerson.java
+++ app/src/main/java/mx/com/diossa/adeudosapp/MainPerson.java
@@ -3,6 +3,7 @@ package mx.com.diossa.adeudosapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,12 +17,15 @@ import android.widget.Toast;
 public class MainPerson extends Activity implements View.OnClickListener  {
 
     public static final String USERNAME = "user";
+    public static final String LOG_TAG = "MainActivity Lifecycle";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.person_xml);
 
+        Log.d(LOG_TAG,"On create");
         Button btn_calculate = (Button)findViewById(R.id.btn_calculate_debt);
         Button btn_exit = (Button)findViewById(R.id.btn_exit_dbt);
         Button btn_loggin = (Button)findViewById(R.id.btn_loggin_debt);
@@ -46,6 +50,42 @@ public class MainPerson extends Activity implements View.OnClickListener  {
 
 
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG,"On Start");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG,"On Restart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG,"On Resume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG,"On Pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG,"On Pause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG,"On Destroy");
     }
 
     @Override
